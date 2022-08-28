877c616b1a82b8324f226e569348054975a77a34
Maksym Kyian <mkyian@foundationmedicine.com>
2022-08-28 23:43:24 +0300

    add unit tests project

---
+++ tests/DecisionHelper.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DecisionHelper.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Fail("test fail");
+        }
+    }
+}
